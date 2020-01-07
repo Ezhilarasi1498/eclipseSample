1caa83485a047c82d576a17d0416fb6c7b93178d
Ezhilarasi1498 <ezhilellappan14@gmail.com>
2020-01-07 16:59:40 +0530

    add files

---
+++ Assignment/src/StringReverse.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class StringReverse {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String");
+		String str = sc.next();
+		String rev ="";
+		for(int i = str.length()-1;i>=0;i--){
+			rev +=str.charAt(i);
+		}
+		//System.out.println("reverse order : "+rev);
+		if(str.equals(rev)){
+			System.out.println("Given string is palindrome");
+		}
+		else{
+			System.out.println("String is not a palindrome");
+		}
+	}
+
+	
+
+}
